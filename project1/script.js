93ec2de4c6fd01c1ac0d4e43b1c05e7f11212a5a
yash-pareek-21 <yp883677@gmail.com>
2025-11-16 23:29:29 +0530

    Delete project1 directory

--- project1/script.js
+++
@@ -1,7 +0,0 @@
-let btn = document.querySelector("#btn1");
-console.log(btn)
-btn.addEventListener("onclick",() =>{
-    window.location.href="https://www.youtube.com/"
-    console.log("clicked")
-})
-
