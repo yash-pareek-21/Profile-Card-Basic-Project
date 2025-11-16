289ec495c892f6d089366a2a10931abd6e0f1483
yash-pareek-21 <yp883677@gmail.com>
2025-11-16 23:43:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let btn = document.querySelector("#btn1");
 console.log(btn)
-btn.addEventListener("onclick",() =>{
+btn.addEventListener("click",() =>{
     window.location.href="https://www.youtube.com/"
     console.log("clicked")
 })
+let rtn = document.querySelector("#btn2");
+console.log(rtn)
+rtn.addEventListener("click",() =>{
+    window.location.href="https://www.instagram.com/reel/DPy8B96Eixu/?igsh=MW44eG9jMXlrdmdtcQ=="
+    console.log("clicked")
+})
+
 
