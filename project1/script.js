9d38f8f57567daccb4bee9a2c769284cc1f73a55
yash-pareek-21 <yp883677@gmail.com>
2025-11-16 23:24:33 +0530

    First project

---
+++ project1/script.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("#btn1");
+console.log(btn)
+btn.addEventListener("onclick",() =>{
+    window.location.href="https://www.youtube.com/"
+    console.log("clicked")
+})
+
